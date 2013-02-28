c9695660f17c2808d5e5a7d794587f67d764697c
Ludwig Schmidt-Hackenberg <ludwig@iupr.com>
2013-02-28 15:24:14 +0100

    corrected pip8 violations

--- sift.py
+++ sift.py
@@ -10,13 +10,13 @@ import os
 from PIL import Image
 from numpy import *
 import numpy
-import time
 import cPickle
 import pylab
 from os.path import exists
 MAXSIZE = 1024
 VERBOSE = True
-WRITE_VERBOSE = False # no verbose reading atm
+WRITE_VERBOSE = False  # no verbose reading atm
+
 
 def process_image(imagename, resultname='temp.sift', dense=False):    
     """ process an image and save the results in a .key ascii file"""
@@ -25,7 +25,7 @@ def process_image(imagename, resultname='temp.sift', dense=False):
         if imagename[-3:] != 'pgm':
             #create a pgm file, image is resized, if it is too big.
             # sift returns an error if more than 8000 features are found
-            size = (MAXSIZE,MAXSIZE)
+            size = (MAXSIZE, MAXSIZE)
             im = Image.open(imagename).convert('L')
             im.thumbnail(size, Image.ANTIALIAS)
             im.save('tmp.pgm')
@@ -33,20 +33,20 @@ def process_image(imagename, resultname='temp.sift', dense=False):
         
         #check if linux or windows 
         if os.name == "posix":
-            cmmd = "./sift <"+imagename+">"+resultname
+            cmmd = "./sift <" + imagename + ">" + resultname
             print cmmd
         else:
-            cmmd = "siftWin32 <"+imagename+">"+resultname
+            cmmd = "siftWin32 <" + imagename + ">" + resultname
         
         os.system(cmmd)
         if os.path.getsize(resultname) == 0:
-            raise IOError("extracting SIFT features failed " +resultname)
-        #print 'processed', imagename
+            raise IOError("extracting SIFT features failed " + resultname)
+
     else:
         import vlfeat
 
         # like in pinto2008 why is vision hard
-        size = (150,150)
+        size = (150, 150)
         window_size = 8
         step = 10
 
@@ -58,63 +58,62 @@ def process_image(imagename, resultname='temp.sift', dense=False):
         elif im_array.ndim == 2:
             im_gray = im_array
         else:
-            raise IOError("Not enough dims found in image " +resultname)
+            raise IOError("Not enough dims found in image " + resultname)
         
 
-        #locs,int_descriptors = vlfeat.vl_dsift(im_gray,size=window_size,verbose=VERBOSE)
-        locs,int_descriptors = vlfeat.vl_dsift(im_gray,step=step,verbose=VERBOSE)
+        locs, int_descriptors = vlfeat.vl_dsift(im_gray, step=step, verbose=VERBOSE)
         nfeatures = int_descriptors.shape[1]
-        padding = numpy.zeros((2,nfeatures))
-        locs = numpy.vstack((locs,padding))
-        header = ' '.join([str(nfeatures),str(128)])
-        temp=int_descriptors.astype('float')  # convert descriptors to float
-        descriptors=temp[:]
-        with open(resultname,'wb') as f:
-            cPickle.dump([locs.T,descriptors.T],f , protocol = cPickle.HIGHEST_PROTOCOL)
+        padding = numpy.zeros((2, nfeatures))
+        locs = numpy.vstack((locs, padding))
+        header = ' '.join([str(nfeatures), str(128)])
+        temp = int_descriptors.astype('float')  # convert descriptors to float
+        descriptors = temp[:]
+        with open(resultname, 'wb') as f:
+            cPickle.dump([locs.T, descriptors.T], f, protocol=cPickle.HIGHEST_PROTOCOL)
         print "features saved in", resultname
         if WRITE_VERBOSE:
             with open(resultname, 'w') as f:
                 f.write(header)
                 f.write("\n")
                 for i in range(nfeatures):
-                    f.write(' '.join(map(str,locs[:,i])))
+                    f.write(' '.join(map(str, locs[:, i])))
                     f.write("\n")
-                    f.write(' '.join(map(str,descriptors[:,i])))
+                    f.write(' '.join(map(str, descriptors[:, i])))
                     f.write("\n")
 
 
-def read_features_from_file(filename='temp.sift',dense = False):
+def read_features_from_file(filename='temp.sift', dense=False):
     """ read feature properties and return in matrix form"""
     
     if exists(filename) != False | os.path.getsize(filename) == 0:
-        raise IOError(filename+" wrong file path or file empty")
+        raise IOError(filename + " wrong file path or file empty")
     if dense == True:
-        with open(filename,'rb') as f:
-            locs,descriptors = cPickle.load(f)
+        with open(filename, 'rb') as f:
+            locs, descriptors = cPickle.load(f)
     else:           
         f = open(filename, 'r')
         header = f.readline().split()
         
-        num = int(header[0]) #the number of features
-        featlength = int(header[1]) #the length of the descriptor
-        if featlength != 128: #should be 128 in this case
-            raise RuntimeError, 'Keypoint descriptor length invalid (should be 128).' 
+        num = int(header[0])  # the number of features
+        featlength = int(header[1])  # the length of the descriptor
+        if featlength != 128:  # should be 128 in this case
+            raise RuntimeError('Keypoint descriptor length invalid (should be 128).')
                  
         locs = zeros((num, 4))
         descriptors = zeros((num, featlength));        
 
         #parse the .key file
-        e =f.read().split() #split the rest into individual elements
+        e = f.read().split()  # split the rest into individual elements
         pos = 0
         for point in range(num):
             #row, col, scale, orientation of each feature
             for i in range(4):
-                locs[point,i] = float(e[pos+i])
+                locs[point, i] = float(e[pos + i])
             pos += 4
             
             #the descriptor values of each feature
             for i in range(featlength):
-                descriptors[point,i] = int(e[pos+i])
+                descriptors[point, i] = int(e[pos + i])
             #print descriptors[point]
             pos += 128
             
@@ -124,11 +123,11 @@ def read_features_from_file(filename='temp.sift',dense = False):
             
         f.close()
     
-    return locs,descriptors
+    return locs, descriptors
 
 
     
-def match(desc1,desc2):
+def match(desc1, desc2):
     """ for each descriptor in the first image, select its match to second image
         input: desc1 (matrix with descriptors for first image), 
         desc2 (same for second image)"""
@@ -136,11 +135,11 @@ def match(desc1,desc2):
     dist_ratio = 0.6
     desc1_size = desc1.shape
     
-    matchscores = zeros((desc1_size[0],1))
-    desc2t = desc2.T #precompute matrix transpose
+    matchscores = zeros((desc1_size[0], 1))
+    desc2t = desc2.T  # precompute matrix transpose
     for i in range(desc1_size[0]):
-        dotprods = dot(desc1[i,:],desc2t) #vector of dot products
-        dotprods = 0.9999*dotprods
+        dotprods = dot(desc1[i, :], desc2t)  # vector of dot products
+        dotprods = 0.9999 * dotprods
         #inverse cosine and sort, return index for features in second image
         indx = argsort(arccos(dotprods))
         
@@ -150,7 +149,8 @@ def match(desc1,desc2):
         
     return matchscores 
     
-def plot_features(im,locs):
+
+def plot_features(im, locs):
     """ show image with features. input: im (image as array), 
         locs (row, col, scale, orientation of each feature) """
     
@@ -160,7 +160,8 @@ def plot_features(im,locs):
     pylab.axis('off')
     pylab.show()
     
-def appendimages(im1,im2):
+
+def appendimages(im1, im2):
     """ return a new image that appends the two images side-by-side."""
     
     #select the image with the fewest rows and fill in enough empty rows
@@ -168,18 +169,19 @@ def appendimages(im1,im2):
     rows2 = im2.shape[0]
     
     if rows1 < rows2:
-        im1 = concatenate((im1,zeros((rows2-rows1,im1.shape[1]))), axis=0)
+        im1 = concatenate((im1, zeros((rows2 - rows1, im1.shape[1]))), axis=0)
     else:
-        im2 = concatenate((im2,zeros((rows1-rows2,im2.shape[1]))), axis=0)
+        im2 = concatenate((im2, zeros((rows1 - rows2, im2.shape[1]))), axis=0)
         
-    return concatenate((im1,im2), axis=1)
+    return concatenate((im1, im2), axis=1)
     
-def plot_matches(im1,im2,locs1,locs2,matchscores):
+
+def plot_matches(im1, im2, locs1, locs2, matchscores):
     """ show a figure with lines joining the accepted matches in im1 and im2
         input: im1,im2 (images as arrays), locs1,locs2 (location of features), 
         matchscores (as output from 'match'). """
     
-    im3 = appendimages(im1,im2)
+    im3 = appendimages(im1, im2)
 
     pylab.gray()
     pylab.imshow(im3)
@@ -187,6 +189,9 @@ def plot_matches(im1,im2,locs1,locs2,matchscores):
     cols1 = im1.shape[1]
     for i in range(len(matchscores)):
         if matchscores[i] > 0:
-            pylab.plot([locs1[i,1], locs2[int(matchscores[i]),1]+cols1], [locs1[i,0], locs2[int(matchscores[i]),0]], 'c')
+            pylab.plot([locs1[i, 1], 
+                       locs2[int(matchscores[i]), 1] + cols1], 
+                       [locs1[i, 0], locs2[int(matchscores[i]), 0]], 
+                       'c')
     pylab.axis('off')
     pylab.show()
